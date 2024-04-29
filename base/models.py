f062187e746f1cad039f9656aa3412b6d8a680a8
Bikram <ambreeshpatel98@gmail.com>
2024-04-30 01:00:19 +0530

    added basemodel

--- base/models.py
+++ base/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(("created_at"), auto_now = False, auto_now_add=True)
+    updated_up = models.DateTimeField(("updated_up"), auto_now= True, auto_now_add=False)
+    
+    class Meta:
+        abstract = True
