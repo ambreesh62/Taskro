49ad7de439427c8e73b5fe48b4a06cd888445c8a
Bikram <ambreeshpatel98@gmail.com>
2024-05-03 23:41:53 +0530

    added task ApiView

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from tasks.views import Sub_TaskAPIView
+from tasks.views import Sub_TaskAPIView, TaskAPIView
 
 urlpatterns = [
     path('subtask', Sub_TaskAPIView.as_view()),
     path('subtask/<int:pk>/', Sub_TaskAPIView.as_view(), name='subtask-detail'),
+    
+    path('task', TaskAPIView.as_view()),
+    path('task/<int:pk>/', TaskAPIView.as_view(), name='task-datail'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -58,15 +58,75 @@ class Sub_TaskAPIView(APIView):
                     print(serializer.errors)
                     return Response({'errors' : serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
                 serializer.save()
-                return Response({'Playload' : serializer.data, 'massage' : 'Your data is up to date'}, status=status.HTTP_201_CREATED) 
+                return Response({'Playload' : serializer.data, 'massage' : 'Data is up to date'}, status=status.HTTP_201_CREATED) 
         except Sub_Task.DoesNotExist:
-            return Response({'massage' : 'Your data DoesNotExist'},status=status.HTTP_400_BAD_REQUEST)        
+            return Response({'massage' : 'Data DoesNotExist'},status=status.HTTP_400_BAD_REQUEST)        
         
     def delete(self, request, pk=None):
         try:
             if pk:
                 sub_tasks = Sub_Task.objects.get(pk=pk)
                 sub_tasks.delete()
-                return Response({'massage' : 'Your data has been deleted'}, status=status.HTTP_200_OK)
+                return Response({'massage' : 'Data has been deleted'}, status=status.HTTP_200_OK)
         except Sub_Task.DoesNotExist:
-            return Response({'massage' : 'Your data DoesNotExist'},status=status.HTTP_400_BAD_REQUEST)        
+            return Response({'massage' : 'Data DoesNotExist'},status=status.HTTP_400_BAD_REQUEST)        
+
+
+class TaskAPIView(APIView):
+    def get(self, request, pk=None):
+        try:
+            if pk:
+                tasks = Task.objects.get(id=pk)
+                serializer = TaskSerializer(tasks)
+                return Response({'Playload' : serializer.data, 'massage' : 'success'}, status=status.HTTP_202_ACCEPTED)
+            tasks = Task.objects.all()
+            serializer = TaskSerializer(tasks, many=True)
+            return Response({'Playlod' : serializer.data, 'massage' : 'success'}, status=status.HTTP_202_ACCEPTED)
+        except Task.DoesNotExist:
+            return Response({'massage' : 'Data DoesNotExist'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+        
+        if not serializer.is_valid():
+            print(serializer.errors)
+            return Response({'errors' : serializer.errors, 'massage' : 'somthing went worng'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer.save()
+        return Response({'Playload' : serializer.data, 'massage' : 'success'}, status=status.HTTP_201_CREATED)
+    
+    def put(self, request, pk=None):
+        try:
+            tasks = Task.objects.get(pk=pk)
+            serializer = TaskSerializer(tasks, data=request.data)
+            
+            if not serializer.is_valid():
+                print(serializer.errors)
+                return Response({'errors' : serializer.errors, 'massage' : 'somthing went wworng'}, status=status.HTTP_400_BAD_REQUEST)
+            serializer.save()
+            return Response({'Playload' : serializer.data, 'massage' : 'Data is up to date'}, status=status.HTTP_201_CREATED)
+        except Task.DoesNotExist:
+            return Response({'errors' : 'Data DoesNotExist'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self, request, pk=None):
+        try:
+            tasks = Task.objects.get(pk=pk)
+            serializer = TaskSerializer(tasks, data=request.data, partial=True)
+        
+            if not serializer.is_valid():
+                print(serializer.errors)
+                return Response({'errors' : serializer.errors, 'massage' : 'somthing went wworng'}, status=status.HTTP_400_BAD_REQUEST)
+            serializer.save()
+            return Response({'Playload' : serializer.data, 'massage' : 'Data is up to date'}, status=status.HTTP_201_CREATED)
+        except Task.DoesNotExist:
+            return Response({'errors' : 'Data DoesNotExist'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        
+    def delete(self, request, pk=None):
+        try:
+            tasks = Task.objects.get(pk=pk)
+            tasks.delete()
+            return Response({'massage' : 'Data has been deleted'}, status=status.HTTP_200_OK)
+        except Task.DoesNotExist:
+            return Response({'massage' : 'Data DoesNotExist'})  
