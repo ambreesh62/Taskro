f5f2131dce67845b2041c0f4e4b48484b5cbc091
Bikram <ambreeshpatel98@gmail.com>
2024-05-15 02:06:02 +0530

    updated serializers

--- user/seralizers.py
+++ user/seralizers.py
@@ -6,8 +6,8 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = '__all__' 
-        # fields = ['username', 'password']
+        # fields = '__all__' 
+        fields = ['username', 'password']
     
     def create(self, validated_data):
         user = User.objects.create(username=validated_data.get('username'))
@@ -24,17 +24,19 @@ class UserSerializer(serializers.ModelSerializer):
         fields = '__all__' 
         
     def update(self, instance, validated_data):
-        password = validated_data.get('password', instance.password)
-        if password:
-            instance.set_password(password)
         instance.username = validated_data.get('username', instance.username)
         instance.first_name = validated_data.get('first_name', instance.first_name)
-        instance.last_name = validated_data.get('lastname', instance.last_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
         instance.email = validated_data.get('email', instance.email)
-        instance.name = validated_data.get(instance.name)
-        instance.role_title = validated_data.get(instance.role_title)    
-        instance.dept = validated_data.get(instance.dept)
-        instance.location = validated_data.get(instance.location)
-        instance.employee_no =validated_data.get(instance.employee_no)
+        instance.name = validated_data.get('name', instance.name)
+        instance.role_title = validated_data.get('role_title', instance.role_title)
+        instance.dept = validated_data.get('dept', instance.dept)
+        instance.location = validated_data.get('location', instance.location)
+        instance.employee_no = validated_data.get('employee_no', instance.employee_no)
+        
+        password = validated_data.get('password')
+        if password:
+            instance.set_password(password)
+
         instance.save()
-        return instance        
+        return instance  
